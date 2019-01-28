df7f88cd5877ca9e6066bfc480b093584bb4380a
Arum Puspa Pratiwi <arum_puspa_26 rpl@student.smktelkom-mlg.sch.id>
2019-01-28 14:24:23 +0700

    selesai

---
+++ application/views/event.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Event</title>
+  </head>
+  <body>
+    <div class="main-content">
+      <h1 style="text-align: center;">Event</h1>
+      <br>
+      <img src="<?=base_url()?>assets/img/run.jpg" alt="" style="width:500px;">
+      <p>Event Lari tahun 2018 untuk Galang Dana Donor Darah yang diselenggarakan oleh Online Shop Kami!</p>
+      <br>
+      <img src="<?=base_url()?>assets/img/ultah.jpg" alt="" style="width:500px;">
+      <p>Acara Ulang Tahun yang ke-10 Online Shop Kami!</p>
+      <br>
+      <img src="<?=base_url()?>assets/img/hmm.jpg" alt="" style="width:500px;">
+      <p>Acara Bakti Sosial tahun 2018 di area Surabaya!</p>
+    </div>
+  </body>
+</html>
---
+++ application/views/gallery.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Gallery</title>
+  </head>
+  <body>
+    <div class="main-content">
+      <h1 style="text-align: center;">Galeri</h1>
+      <br>
+      <img src="<?=base_url()?>assets/img/cintah.jpg" alt="" style="width:500px;">
+      <p>Seru-seruan Bareng!</p>
+      <br>
+      <img src="<?=base_url()?>assets/img/iku.jpg" alt="" style="width:500px;">
+      <p>Seru-seruan Bareng!</p>
+      <br>
+    </div>
+
+  </body>
+</html>
